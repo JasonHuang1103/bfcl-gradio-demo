1dc51b7a2dcf55e7b7eea1a2cdd917c3d20a4c03
Jason Huang <jasonhuang1103@berkeley.edu>
2025-02-28 15:40:47 -0800

    deleted unnecessray files

--- standardize 2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def get_result(result_path):
-    """
-    Read CSV file and return DataFrame with relevant columns
-    """
-    df = pd.read_csv(result_path)
-    return df[['entry_id', 'error_reason_type', 'error_reason_description', 'error_turn_id']]
